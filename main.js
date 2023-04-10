fbd4fa1373dab794293ad235f52c975989dfd120
maashh <58293726+maashhCS@users.noreply.github.com>
2023-04-11 00:08:10 +0200

    fixed a bug that wouldn't let you create new Books after you deleted a Book

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let myLibrary = [];
 let bookId = 0;
 
+
 const inputTitle = document.querySelector('#title');
 const inputAuthor = document.querySelector('#author');
 const inputPages = document.querySelector('#pages');
@@ -19,6 +20,7 @@ function Book(title, author, pages, read){
 //Adds the created book Object to the myLibrary Array
 function addBookToLibrary(book) {
     myLibrary.push(book);
+    bookId++;
 }
 
 // Creates a new Book with the Book Constructor
@@ -30,80 +32,71 @@ function createBook(title, author, pages, read){
 
 //Creates a div that shows the Object values of the book
 const bookRow = document.querySelector('.book-row');
-function showBooksOnSite(index){
-    //DOM variables
-    const newBookDiv = document.createElement('div');
-    const newBookTitle = document.createElement('h1');
-    const newBookAuthor = document.createElement('h1');
-    const newBookPages = document.createElement('h1');
-    const newBookRead = document.createElement('h1');
-    const newDeleteButton = document.createElement('button');
-    const newToggleReadButton = document.createElement('button');
-    const newButtonDiv = document.createElement('div');
+function showBooksOnSite(){
+    for(let i = 0; i < myLibrary.length; i++){
 
-    //Add class
-    newBookDiv.classList.add('book-div');
-    newBookTitle.classList.add(`book-title`);
-    newBookAuthor.classList.add(`book-author`);
-    newBookPages.classList.add(`book-pages`);
-    newBookRead.classList.add(`book-read`);
-    newButtonDiv.classList.add('book-button-div');
-    newDeleteButton.classList.add('btn');
-    newToggleReadButton.classList.add('btn');
+        //DOM variables
+        const newBookDiv = document.createElement('div');
+        const newBookTitle = document.createElement('h1');
+        const newBookAuthor = document.createElement('h1');
+        const newBookPages = document.createElement('h1');
+        const newBookRead = document.createElement('h1');
+        const newDeleteButton = document.createElement('button');
+        const newToggleReadButton = document.createElement('button');
+        const newButtonDiv = document.createElement('div');
 
-    //Add id
-    newDeleteButton.setAttribute('id','delete-btn');
-    newToggleReadButton.setAttribute('id','toggle-read-btn');
+        //Add class
+        newBookDiv.classList.add('book-div');
+        newBookTitle.classList.add(`book-title`);
+        newBookAuthor.classList.add(`book-author`);
+        newBookPages.classList.add(`book-pages`);
+        newBookRead.classList.add(`book-read`);
+        newButtonDiv.classList.add('book-button-div');
+        newDeleteButton.classList.add('btn');
+        newToggleReadButton.classList.add('btn');
 
-    //Change text content
-    newBookTitle.textContent = `${myLibrary[index].title}`;
-    newBookAuthor.textContent = `By ${myLibrary[index].author}`;
-    newBookPages.textContent = `Pages ${myLibrary[index].pages}`;
-    if(myLibrary[index].read === true){
-        newBookRead.textContent = `Read: Yes`;
-    } else {
-        newBookRead.textContent = `Read: No`;
+        //Add id
+        newDeleteButton.setAttribute('id', `delete-btn-${i}`);
+        newToggleReadButton.setAttribute('id', `toggle-read-btn-${i}`);
+
+        //Change text content
+        newBookTitle.textContent = `${myLibrary[i].title}`;
+        newBookAuthor.textContent = `By: ${myLibrary[i].author}`;
+        newBookPages.textContent = `Pages: ${myLibrary[i].pages}`;
+        if(myLibrary[i].read === true){
+            newBookRead.textContent = `Read: Yes`;
+        } else {
+            newBookRead.textContent = `Read: No`;
+        }
+        newDeleteButton.textContent = "Delete";
+        newToggleReadButton.textContent = "Change Status";
+
+        //adds the nodes as the last child of the parent element
+        bookRow.appendChild(newBookDiv);
+        newBookDiv.appendChild(newBookTitle);
+        newBookDiv.appendChild(newBookAuthor);
+        newBookDiv.appendChild(newBookPages);
+        newBookDiv.appendChild(newBookRead);
+        newBookDiv.appendChild(newButtonDiv);
+        newButtonDiv.appendChild(newDeleteButton);
+        newButtonDiv.appendChild(newToggleReadButton);
+
     }
-    newDeleteButton.textContent = "Delete";
-    newToggleReadButton.textContent = "Change Status";
 
-    //
-    bookRow.appendChild(newBookDiv);
-    newBookDiv.appendChild(newBookTitle);
-    newBookDiv.appendChild(newBookAuthor);
-    newBookDiv.appendChild(newBookPages);
-    newBookDiv.appendChild(newBookRead);
-    newBookDiv.appendChild(newButtonDiv);
-    newButtonDiv.appendChild(newDeleteButton);
-    newButtonDiv.appendChild(newToggleReadButton);
-
-    //Delete and change status button EventListener
+    //Delete Book and change status button EventListener
     const bookDivButtons = document.querySelectorAll('.book-button-div > button');
     bookDivButtons.forEach(btn => {
         btn.addEventListener('click', (e) => {
-            if(e.target.matches('#delete-btn')){
-                for(let i = 0; i < myLibrary.length; i++){
-                    console.log(myLibrary[i]);
-                    if(e.target.parentElement.parentElement.children[0].innerText === myLibrary[i].title){
-                        const deleteDiv = e.target.parentElement.parentElement;
-                        myLibrary.splice(i, 1);
-                        deleteDiv.remove(); 
-                    }
-                }
-            } else if(e.target.matches('#toggle-read-btn')){
-                for(let i = 0; i < myLibrary.length; i++){
-                    if(e.target.parentElement.parentElement.children[0].innerText === myLibrary[i].title){
-                        console.log(e);
-                        console.log(myLibrary[i].read);
-                        if(myLibrary[i].read === true){
-                            myLibrary[i].read = false;
-                            console.log(myLibrary[i].read);
-                            e.target.parentElement.parentElement.children[3].innerText = 'Read: No'
-                        } else {
-                            myLibrary[i].read = true;
-                            console.log(myLibrary[i].read);
-                            e.target.parentElement.parentElement.children[3].innerText = 'Read: Yes'
-                        }
+            for(let i = 0; i < myLibrary.length; i++){
+                if(e.target.matches(`#delete-btn-${i}`)){
+                    deleteBook(i);
+                } else if(e.target.matches(`#toggle-read-btn-${i}`)){
+                    if(myLibrary[i].read){
+                        myLibrary[i].read = false;
+                        e.target.parentElement.parentElement.children[3].innerText = 'Read: No'
+                    }else {
+                        myLibrary[i].read = true;
+                        e.target.parentElement.parentElement.children[3].innerText = 'Read: Yes'
                     }
                 }
             }
@@ -124,35 +117,43 @@ bookButton.addEventListener('click', (e) => {
     }
 })
 
-//Submits the data to the createBook function
+//Submits the data to the createBook function and displays it on the page
 const submitButton = document.querySelector('#submit-book-btn');
 submitButton.addEventListener('click', (e) => {
     e.preventDefault();
     createBook(inputTitle.value, inputAuthor.value, inputPages.value, inputReadYes.checked);
     clearInput();
     formDiv.style.display = 'none';
-    showBooksOnSite(bookId);
     bookButton.textContent = 'Create Book';
-    bookId++;
+    deleteDiv();
+    showBooksOnSite();
 })
 
 //Clears the input fields
 function clearInput(){
     inputTitle.value = "";
     inputAuthor.value = "";
-    inputPages.value = 0;
+    inputPages.value = "";
     inputReadNo.checked = true;
 }
 
-//Loops through the myLibrary array and creates a new Book div on the page for every Book Object in the Array
-function loopLibrary(){
-    for(let i = 0; i < myLibrary.length; i++){
-        const deleteBookRowChildren = document.querySelector('.book-row');
-        let delChildren = deleteBookRowChildren.lastChild;
-        while(delChildren){
-            deleteBookRowChildren.removeChild(delChildren);
-            delChildren = deleteBookRowChildren.lastChild;
-        }
-        showBooksOnSite(i);
+//Deletes every child inside the div 'book-row'
+function deleteDiv(){
+    if(myLibrary.length < 1){
+        let delChildren = bookRow.lastChild;
+        bookRow.removeChild(delChildren);
     }
+    for(let i = 0; i < myLibrary.length; i++){
+        let delChildren = bookRow.lastChild;
+        while(delChildren){
+            bookRow.removeChild(delChildren);
+            delChildren = bookRow.lastChild;
+        }
+    }
+}
+
+function deleteBook(index){
+    myLibrary.splice(index, 1);
+    deleteDiv();
+    showBooksOnSite();
 }
